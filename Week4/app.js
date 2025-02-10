a090f05f840b50bee1b6fad74d44069b9e01f7c6
Phakaphol Dherachaisuphakij <godzk25@gmail.com>
2025-02-10 10:28:44 +0700

    com

---
+++ Week4/app.js
@@ -0,0 +1,41 @@
+let ab = 42
+let ba = "42"
+console.log(ab == ba);
+
+function compare(){
+    let a = 5
+    const b = 10
+    var c = 20
+}
+console.log(`let ${compare.c} const ${compare.b} var is ${compare.c}`)
+
+
+let x = "hello"
+console.log(x);
+x = 18
+console.log(x);
+
+let a = 10; 
+let b = a;  
+b = 20;     
+console.log(a);
+console.log(b); 
+
+let refa = [1,2,3]
+let refb = refa
+
+refb[1] = "Hello"
+console.log(refa);
+
+
+// let y = 20
+// let y = 0
+// Error !!!
+// var z = 0
+// var z = 10
+
+
+
+let cronos = 18
+let cronas = 18
+console.log(cronas == cronos);
